0c76dd308435a85a7696e4b14fb4abf7b9393369
samja02 <jaschke.samuel13@gmail.com>
2023-03-23 15:31:01 +0100

    Aenderungen der Klassen

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
-<?php
-// Ein einfaches "Hello World" Programm in PHP
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Infotool</title>
+    <link rel="stylesheet" type="text/css" href="format.css">
+</head>
+
+<body>
+    <!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./styles/styles.css">
+        <title> PC und Zubehoer GmbH</title>
+    </head>
+    <body>
+        <div id="head">
+            <?php require './texte/head.htm';?> 
+        </div>
+        <div id="nav">
+            <?php require 'navi.php'; ?>
+        </div>
+        <div id="main">
+            <?php require $main;?> 
+        </div>
+
+    </body>
+</html>
+
+</body>
+
+</html>
